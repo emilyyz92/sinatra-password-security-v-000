81d3a1f8a6ec6f00f0fabc4f22f3feae2b3f337a
Emily Zhao <tong.zhao.92@gmail.com>
2018-05-30 19:49:29 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,8 +32,8 @@ class ApplicationController < Sinatra::Base
 	post "/login" do
 		user = User.find_by(:username => params[:username])
 		if user && user.authenticate(params[:password])
-			session[:user_id] = user.idea
-			redirect '/success'
+			session[:user_id] = user.id
+			redirect "/success"
 		else
 			redirect '/failure'
 		end
